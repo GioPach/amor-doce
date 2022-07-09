d281e171f7522bf44ac6ee804632f4de6e897cd9
Gabriela Estigarribia <gestigarribia28@gmail.com>
2022-07-09 16:04:37 -0300

    Implementa todas telas fxml personagens

---
+++ src/amordoce/control/personagens/caioLopes/TelaLogCaioLopesController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package amordoce.control.personagens.caioLopes;
+
+import amordoce.App;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author gisig
+ */
+public class TelaLogCaioLopesController implements Initializable {
+    
+    public ObservableList<String> listaPerguntasRespostas;
+    
+    @FXML
+    public ListView<String> listView;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    @FXML
+    public void initialize(URL url, ResourceBundle rb) {
+        this.listaPerguntasRespostas = TelaCaioLopesController.ema.logPersonagem();
+        listView.setItems(listaPerguntasRespostas);
+    }    
+    
+    @FXML
+    private void handlerButtonVoltar(ActionEvent event) throws Exception {
+        App.setRoot("personagens/emaMontenegro/TelaEmaMontenegro");
+    }
+    
+  
+    
+}
